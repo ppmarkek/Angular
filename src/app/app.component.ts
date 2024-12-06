64c53d6b14ab461dd7bea62e3fc4adbcaaf58a40
pppmarkek <153396079+ppmarkek@users.noreply.github.com>
2024-12-06 12:34:55 +0200

    feat: ViewChild and ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  TemplateRef,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChildComponent } from "./components/child/child.component";
+import { ChildComponent } from './components/child/child.component';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +16,25 @@ import { ChildComponent } from "./components/child/child.component";
   imports: [CommonModule, ChildComponent],
 })
 export class AppComponent {
-  toggler: boolean = true
-  title: string = 'aboba'
+  title: string = 'Title';
+
+  // @ViewChild('selector') property: any;
+  // @ViewChild('paragrath') paragrath!: ElementRef<HTMLParagraphElement>;
+  // @ViewChild('paragrath', { read: ChildComponent, static: true })
+  // paragrath!: ElementRef<HTMLParagraphElement>;
+  // @ViewChild('template') template!: TemplateRef<HTMLParagraphElement>;
+  // @ViewChild('component') component!: ChildComponent;
+
+  @ViewChildren('paragrath')
+  paragrath!: QueryList<HTMLParagraphElement>;
+  @ViewChildren('template') template!: QueryList<HTMLParagraphElement>;
+  @ViewChildren('component') component!: QueryList<ChildComponent>;
+
+  ngAfterViewInit() {
+    console.log('paragrath', this.paragrath);
+    console.log('template', this.template);
+    console.log('component', this.component);
+
+    // this.paragrath.nativeElement.textContent = 'aboba';
+  }
 }
